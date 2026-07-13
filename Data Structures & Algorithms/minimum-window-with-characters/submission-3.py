a5676ea595b5bc4d1263cef18f5fc9c6bb02aba8
Nakshh Kohli <35568319+Nakshh@users.noreply.github.com>
2026-07-13 22:04:46 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        right = 0
+        answer = ""
+
+        if len(t) > len(s):
+            return ""
+
+        for i, c in enumerate(s):
+            if c in t:
+                right = i + len(t)
+                while right <= len(s):
+                    if Counter(t) <= Counter(s[i:right]):
+                        answer = s[i:right] if answer == "" or len(s[i:right]) < len(answer) else answer
+                        break
+                    else:
+                        right += 1
+                    
+        return answer
+                
+                        
